6e6b0a2b92ba3f63bf8134f40796fc79df1e9de9
liuxinrui5257 <xxl180033@utdallas.edu>
2023-08-03 18:59:33 -0500

    updated changes

--- SimpleClient.py
+++ SimpleClient.py
@@ -1,12 +1,11 @@
 import socket
-import time
 
-# Default IP Address for h1 being the server
-serverAddr = "10.0.0.1"
+HOST, PORT = '10.0.0.1', 4488
 
-clientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Attempting connection to " + serverAddr)
-clientSock.connect((serverAddr, 8000))
-time.sleep(1)
-print("Closing connection and exiting...")
-clientSock.close()
+while True:
+    command = input('Enter command: ')
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        sock.sendall(command.encode())
+        response = sock.recv(1024)
+        print("Received: ", response.decode())
