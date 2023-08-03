1b3531e656c83bfa6c27ecb367927c31b2207bfa
liuxinrui5257 <xxl180033@utdallas.edu>
2023-08-03 18:59:36 -0500

    changes

--- SimpleServer.py
+++ SimpleServer.py
@@ -1,12 +1,34 @@
-import socket
+import socketserver
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('', 8000))
-print("Socket bound to " + sock.getsockname()[0])
-sock.listen()
-print("Server listening...")
+# Define initial states
+devices = {
+    'FR': 'OFF',
+    'K': 'OFF',
+    'T': 70
+}
 
-while True:
-    connectionSocket, addr = sock.accept()
-    print("CONNECTED SOURCE: " + str(addr))
-    connectionSocket.close()
+class SDCPRequestHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print(f"Received {self.data} from {self.client_address[0]}")
+
+        message = self.data.decode().split()
+
+        if message[0] == 'STATUS':
+            if message[1] == 'ALL':
+                response = '\n'.join([f"{device} {state}" for device, state in devices.items()])
+            else:
+                response = f"{message[1]} {devices.get(message[1], 'UNKNOWN')}"
+        elif message[0] == 'SET':
+            devices[message[1]] = message[2]
+            response = f"{message[1]} set to {message[2]}"
+        else:
+            response = "INVALID COMMAND"
+
+        self.request.sendall(response.encode())
+
+if __name__ == "__main__":
+    HOST, PORT = '10.0.0.1', 4488
+    server = socketserver.TCPServer((HOST, PORT), SDCPRequestHandler)
+    print(f"Server running on {HOST}:{PORT}")
+    server.serve_forever()
